34937f180c37a325b182994b7b375fca5f446b60
Huzaifa-Md <huzaifahussain10@gmail.com>
2026-08-13 04:53:49 +0530

    Add HomePage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+export default function Navbar(){
+    return(
+    <nav className="navbar">
+        <div className="navbar-container">
+            <Link to="/" className="navbar-brand">
+            ShopHub
+            </Link>
+            <div className="navbar-links">
+                <Link to="/" className="navbar-links">Home</Link>
+                <Link to="/Checkout" className="navbar-links">Cart</Link>
+            </div>
+            <div className="navbar-auth">
+                <div className="navbar-auth-links">
+                    <Link to="/Auth" className="btn btn-secondary"> Login</Link>
+                    <Link to="/Auth" className="btn btn-primary"> SignUp</Link>
+                </div>
+            </div>
+        </div>
+    </nav>
+);
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+
+import ProductCard from "../components/productcard";
+import { getProducts } from "../data/products";
+export default function Home(){
+    const products = getProducts();
+    return(
+    <div className="page">
+        <div className="home-hero">
+            <h1 className="home-title"> Welcome to Shophub</h1>
+            <p className="home-subtitle">Discover Amazing Products at lowest prices</p>
+        </div>
+        <div className="container">
+            <h2 className="page-title">Our Products</h2>
+            <div className="product-grid">
+                {products.map((product)=>(
+                    <ProductCard product={product} key ={product.id}/>
+                ))}
+            </div>
+        </div>
+    </div>
+    );
+}
