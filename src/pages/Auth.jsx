e39edf962a0d861818afb43d4f7e01a99b703bb5
Huzaifa-Md <huzaifahussain10@gmail.com>
2026-08-14 01:49:44 +0530

    Authenticationc

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,22 +1,39 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form"
-
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 export default function Auth(){
-    const [mode, setMode] = useState("signup")
+    const [mode, setMode] = useState("signup");
+    const [error, setError] = useState(null);
+    const {signup,login} = useAuth();
+    const navigate = useNavigate();
     const {register,
             handleSubmit,
             formState:{errors},}= useForm();
-    function onSubmit(){
-        alert:("signed up");
+
+    function onSubmit(data){
+        setError(null);
+        let result;
+        if(mode==="signup"){
+            result = signup(data.email, data.password)
+        } else{
+            result= login(data.email, data.password)
+        }        
+        if(result.success){
+            navigate("/");
+        } else{
+            setError(result.error);
+        }
     }
     return(
         <div className="page">
             <div className="container">
                 <div className="auth-container">
                     <h1 className="page-title">
-                        {mode==="signup"? "Sign Up": "Login"}
+                        {mode==="signup"? "Sign Up": "login"}
                     </h1>
                     <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                        {error && <div className="error-message">{error}</div>}
                         <div className="form-group">
                             <label className="form-label" htmlFor="email">
                                 Email
@@ -30,14 +47,14 @@ export default function Auth(){
                             <label className="form-label" htmlFor="password">
                                 Password
                             </label>
-                            <input className="form-input" type="password" id="password" {...register("password",{required:"Password is reuired",
+                            <input className="form-input" type="password" id="password" {...register("password",{required:"Password is required",
                                 minLength: {
                                     value:6,
-                                    message: "password must be atleast 6 characters",
+                                    message: "Password must be atleast 6 characters",
                                 },
                                 maxLength:{
                                     value:12,
-                                    message:"password must be atmost 12 characters",
+                                    message:"Password must be atmost 12 characters",
                                 }
                             })} />
                             {errors.password&&(
@@ -45,14 +62,14 @@ export default function Auth(){
                             )}
                         </div>
                         <button className="btn btn-primary btn-large" type="submit"> 
-                            {mode==="signup"?"Sign Up": "Login"}</button>
+                            {mode==="signup"?"Sign Up": "login"}</button>
                     </form>
                     <div className="auth-switch">
                         {mode==="signup"?(
-                            <p>Already Have an Account?{""}
-                            <span className="auth-link" onClick={()=> setMode("Login")}>Login</span></p>
+                            <p>Already Have an Account?{" "}
+                            <span className="auth-link" onClick={()=> setMode("login")}>Login</span></p>
                         ):(
-                            <p>Dont have an account?{""}
+                            <p>Dont have an account?{" "}
                             <span className="auth-link" onClick={()=> setMode("signup")}>Sign Up</span></p>
                         )}
                     </div>
